2e32d26d51f3cfec642fec3b9f594c406b55199f
Brian Lalonde <brianary@gmail.com>
2026-08-04 21:35:51 -0700

    ✅ Add tests for Get-UnicodeName
    resolves #11

--- test/Get-UnicodeName.Tests.ps1
+++ test/Get-UnicodeName.Tests.ps1
@@ -1,35 +1,30 @@
 ﻿<#
 .SYNOPSIS
-Tests Returns the name of a Unicode code point.
+Tests returning the name of a Unicode code point.
 #>
 
-return #TODO: Write test.
 if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
 	&"$PSScriptRoot/../scripts/Import-ThisModule.ps1"
 }
 Describe 'Get-UnicodeName' -Tag Get-UnicodeName,Get,UnicodeName {
-	Context 'Returns the name of a Unicode code point.' -Tag Example {
-		It "EXAMPLE 1" -Skip {
-			Get-UnicodeName 32 |Should -BeExactly @"
-SPACE
-"@
+	Context 'Returns the name of a Unicode code point.' {
+		It "Given code point number '<CodePoint>' should return '<Result>'" -TestCases @(
+			@{ CodePoint = 32; Result = 'SPACE' }
+			@{ CodePoint = 0x1F570; Result = 'MANTELPIECE CLOCK' }
+			@{ CodePoint = 0x1F33F; Result = 'HERB' }
+		) {
+			Param([int]$CodePoint,[string]$Result)
+			Get-UnicodeName -CodePoint $CodePoint |Should -BeExactly $Result
 		}
-	}
-	Context 'CodePoint' -Tag CodePoint {
-		It "test" -Skip {
-			1 |Should -Be 1
-		}
-	}
-	Context 'Character' -Tag Character {
-		It "test" -Skip {
-			1 |Should -Be 1
-		}
-	}
-	Context 'Update' -Tag Update {
-		It "test" -Skip {
-			1 |Should -Be 1
+		It "Given character '<Character>' should return '<Result>'" -TestCases @(
+			@{ Character = ' '; Result = 'SPACE' }
+			@{ Character = '-'; Result = 'HYPHEN-MINUS' }
+			@{ Character = 'A'; Result = 'LATIN CAPITAL LETTER A' }
+		) {
+			Param([string]$Character,[string]$Result)
+			Get-UnicodeName -Character $Character |Should -BeExactly $Result
 		}
 	}
 }
