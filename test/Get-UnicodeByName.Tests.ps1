e97de754531f86a0a2a27e1246402ecc208fe4df
Brian Lalonde <brianary@gmail.com>
2026-08-08 16:18:34 -0700

    ✨ Unicode scripts: Add option to output characters as text (not emoji)
    - resolves #16
    - resolves #7
    - resolves #8
    - resolves #9

--- test/Get-UnicodeByName.Tests.ps1
+++ test/Get-UnicodeByName.Tests.ps1
@@ -1,50 +1,33 @@
 ﻿<#
 .SYNOPSIS
-Tests Returns characters based on Unicode code point name, GitHub short code, or HTML entity.
+Tests returning characters based on Unicode code point name, GitHub short code, or HTML entity.
 #>
 
-return #TODO: Write test.
 if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
 	&"$PSScriptRoot/../scripts/Import-ThisModule.ps1"
 }
 Describe 'Get-UnicodeByName' -Tag Get-UnicodeByName,Get,UnicodeByName {
-	Context 'Returns characters based on Unicode code point name, GitHub short code, or HTML entity.' -Tag Example {
-		It "EXAMPLE 1" -Skip {
-			Get-UnicodeByName hyphen-minus |Should -BeExactly @"
--
-"@
-		}
-		It "EXAMPLE 2" -Skip {
-			Get-UnicodeByName slash |Should -BeExactly @"
-/
-"@
-		}
-		It "EXAMPLE 3" -Skip {
-			Get-UnicodeByName :zero: |Should -BeExactly @"
-[0]
-"@
-		}
-		It "EXAMPLE 4" -Skip {
-			Get-UnicodeByName '&amp;' |Should -BeExactly @"
-&
-"@
-		}
-		It "EXAMPLE 5" -Skip {
-			Get-UnicodeByName BEL |Should -BeExactly @"
-(beeps)
-"@
-		}
-	}
-	Context 'Name' -Tag Name {
-		It "test" -Skip {
-			1 |Should -Be 1
-		}
-	}
-	Context 'Update' -Tag Update {
-		It "test" -Skip {
-			1 |Should -Be 1
+	Context 'Returns characters based on Unicode code point name, GitHub short code, or HTML entity.' {
+		It "Given the name '<Name>', returns '<Result>'" -TestCases @(
+			@{ Name = 'hyphen-minus'; Result = '-' }
+			@{ Name = 'slash'; Result = '/' }
+			@{ Name = ':zero:'; Result = '0️⃣' }
+			@{ Name = '&amp;'; Result = '&' }
+			@{ Name = 'HT'; Result = "`t" }
+			@{ Name = 'FLOWER PUNCTUATION MARK'; Result = '⁕' }
+			@{ Name = 'waning_gibbous_moon_symbol'; Result = '🌖' }
+		) {
+			Param([string]$Name, [string]$Result)
+			Get-UnicodeByName $Name |Should -BeExactly $Result -Because 'the positional parameter should work'
+			$Name |Get-UnicodeByName |Should -BeExactly $Result -Because 'the pipeline should work'
+			$suffix = "$([char]0xFE0F)" # VS16
+			Get-UnicodeByName $Name -AsEmoji |Should -BeExactly "$Result$suffix" -Because 'the positional parameter should work, as emoji'
+			$Name |Get-UnicodeByName -AsEmoji |Should -BeExactly "$Result$suffix" -Because 'the pipeline should work, as emoji'
+			$suffix = "$([char]0xFE0E)" # VS15
+			Get-UnicodeByName $Name -AsText |Should -BeExactly "$Result$suffix" -Because 'the positional parameter should work, as text'
+			$Name |Get-UnicodeByName -AsText |Should -BeExactly "$Result$suffix" -Because 'the pipeline should work, as text'
 		}
 	}
 }
